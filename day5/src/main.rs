e3b9008ef17f8c2c618547223ed9957d1713180c
Thorsten Ball <mrnugget@gmail.com>
2018-12-17 10:25:11 +0100

    Make use of mutability in day 5 to speed it up by orders of magnitude

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -7,40 +7,22 @@ fn units_can_react(a: char, b: char) -> bool {
     a != b && a.to_lowercase().to_string() == b.to_lowercase().to_string()
 }
 
-fn remove_first_reacting_units(units: Vec<char>) -> (Vec<char>, bool) {
-    let mut result = Vec::new();
-    let mut units_iter = units.iter().peekable();
-    let mut units_removed = false;
+fn react_all(units: &mut Vec<char>) {
+    let mut len = units.len();
+    let mut i = 1;
 
-    while let Some(&current) = units_iter.next() {
-        let cloned = current.clone();
-
-        if let Some(&&peek) = units_iter.peek() {
-            if !units_removed && units_can_react(current, peek) {
-                units_iter.next();
-                units_removed = true;
-            } else {
-                result.push(cloned);
+    while i < len {
+        if units_can_react(units[i - 1], units[i]) {
+            units.remove(i - 1);
+            units.remove(i - 1);
+            if i > 1 {
+                i -= 1;
             }
-        } else {
-            result.push(cloned);
+            len -= 2;
+            continue;
         }
+        i += 1;
     }
-
-    (result, units_removed)
-}
-
-fn remove_all_reacting_units(units: Vec<char>) -> Vec<char> {
-    let mut result = units;
-    let mut units_removed = true;
-
-    while units_removed {
-        let (res, rem) = remove_first_reacting_units(result);
-        result = res;
-        units_removed = rem;
-    }
-
-    result
 }
 
 #[cfg(test)]
@@ -48,30 +30,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_remove_no_units() {
-        let input: Vec<char> = "aaaaa".chars().collect();
-
-        let (_, units_removed) = remove_first_reacting_units(input);
-        assert!(!units_removed);
-    }
-
-    #[test]
-    fn test_remove_first_reacting_units() {
-        let input: Vec<char> = "dabAcCaCBAcCcaDA".chars().collect();
-        let expected: Vec<char> = "dabAaCBAcCcaDA".chars().collect();
-
-        let (result, units_removed) = remove_first_reacting_units(input);
-        assert!(units_removed);
-        assert_eq!(result, expected);
-    }
-
-    #[test]
     fn test_remove_all_reacting_units() {
-        let input: Vec<char> = "dabAcCaCBAcCcaDA".chars().collect();
+        let mut input: Vec<char> = "dabAcCaCBAcCcaDA".chars().collect();
         let expected: Vec<char> = "dabCBAcaDA".chars().collect();
 
-        let result = remove_all_reacting_units(input);
-        assert_eq!(result, expected);
+        react_all(&mut input);
+        assert_eq!(input, expected);
     }
 
     #[test]
@@ -97,11 +61,10 @@ fn main() -> Result<(), std::io::Error> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    let mut units: Vec<char> = contents.chars().collect();
-    units.remove(units.len() - 1);
-    let result = remove_all_reacting_units(units);
+    let mut units: Vec<char> = contents.chars().filter(|x| *x != '\n').collect();
+    react_all(&mut units);
 
-    println!("units left: {}", result.len());
+    println!("units left: {}", units.len());
 
     Ok(())
 }
