ebc201a24d08f783c05442fbc159768ecf1bface
Thorsten Ball <mrnugget@gmail.com>
2018-12-18 07:08:06 +0100

    Fix day 7 part 1

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,33 +1,105 @@
 extern crate regex;
 
 use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
 
-#[derive(Debug)]
-struct Node<'a> {
-    name: &'a str,
-    parent: Option<&'a Node<'a>>,
-    children: Vec<&'a Node<'a>>,
+fn build_graph<'a, I>(lines: I) -> HashMap<&'a str, Vec<&'a str>>
+where
+    I: Iterator<Item = &'a str>,
+{
+    let line_re: Regex = Regex::new(r"Step\s(\w)\smust\sbe\sfinished\sbefore\sstep\s(\w)").unwrap();
+    let mut graph: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in lines {
+        let caps = line_re.captures(line).unwrap();
+        let parent = caps.get(1).unwrap().as_str();
+        let child = caps.get(2).unwrap().as_str();
+        graph.entry(parent).or_default();
+        graph.entry(child).or_default().push(parent);
+    }
+
+    graph
 }
 
-fn find_or_insert<'a>(nodes: &'a mut Vec<Node<'a>>, name: &'a str) -> &'a Node<'a> {
-    match nodes.iter().find(|n| n.name == name) {
-        Some(n) => n,
-        None => {
-            let n = Node {
-                name: name,
-                parent: None,
-                children: Vec::new(),
-            };
-            nodes.push(n);
-            nodes.last().unwrap()
+fn get_next_steps<'a>(
+    steps: &HashSet<&'a str>,
+    graph: &HashMap<&'a str, Vec<&'a str>>,
+    satisfied: &HashSet<&'a str>,
+) -> Vec<&'a str> {
+    let mut result: Vec<&str> = steps
+        .iter()
+        .cloned()
+        .filter(|step| match graph.get(*step) {
+            Some(deps) => deps.iter().all(|dep| satisfied.contains(dep)),
+            None => true,
+        })
+        .collect();
+    result.sort();
+    result
+}
+
+fn build_order(graph: HashMap<&str, Vec<&str>>) -> String {
+    let mut steps: HashSet<&str> = graph.keys().cloned().collect();
+    let mut result: Vec<&str> = Vec::new();
+    let mut satisfied: HashSet<&str> = HashSet::new();
+
+    while !steps.is_empty() {
+        if let Some(&step) = get_next_steps(&steps, &graph, &satisfied).first() {
+            satisfied.insert(step);
+            result.push(step);
+            steps.remove(step);
         }
     }
+
+    result.into_iter().collect::<String>()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_input<'a>() -> Vec<&'a str> {
+        vec![
+            "Step C must be finished before step A can begin.",
+            "Step C must be finished before step F can begin.",
+            "Step A must be finished before step B can begin.",
+            "Step A must be finished before step D can begin.",
+            "Step B must be finished before step E can begin.",
+            "Step D must be finished before step E can begin.",
+            "Step F must be finished before step E can begin.",
+        ]
+    }
+
+    #[test]
+    fn test_building_graph() {
+        let input = test_input();
+        let graph = build_graph(input.into_iter());
+        let mut keys: Vec<&str> = graph.keys().cloned().collect();
+        keys.sort();
+        assert_eq!(keys, vec!["A", "B", "C", "D", "E", "F"]);
+
+        assert_eq!(graph[&"C"].len(), 0);
+        assert_eq!(graph[&"A"], vec!["C"]);
+        assert_eq!(graph[&"B"], vec!["A"]);
+        assert_eq!(graph[&"E"], vec!["B", "D", "F"]);
+        assert_eq!(graph[&"D"], vec!["A"]);
+        assert_eq!(graph[&"F"], vec!["C"]);
+    }
+
+    #[test]
+    fn test_ordering() {
+        let input = test_input();
+        let graph = build_graph(input.into_iter());
+        let order = build_order(graph);
+
+        assert_eq!(order, "CABDFE");
+    }
+}
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
 
@@ -41,46 +113,8 @@ fn main() -> Result<(), std::io::Error> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    let line_re: Regex = Regex::new(r"Step\s(\w)\smust\sbe\sfinished\sbefore\sstep\s(\w)").unwrap();
-
-    let _relationships = contents.lines().fold(Vec::new(), |mut acc, line| {
-        let caps = line_re.captures(line).unwrap();
-        let parent = caps.get(1).unwrap().as_str();
-        let child = caps.get(2).unwrap().as_str();
-        acc.push((parent, child));
-        acc
-    });
-
-    // let mut nodes: Vec<Node> = Vec::new();
-    //
-    // for (p, c) in relationships.iter() {
-    //     match nodes.iter().find(|n| n.name == *p) {
-    //         None => {
-    //             let child = match nodes.iter().find(|n| n.name == *c) {
-    //                 None => {
-    //                     nodes.push(Node {
-    //                         name: *c,
-    //                         parent: None,
-    //                         children: Vec::new(),
-    //                     });
-    //                     nodes.last().unwrap()
-    //                 }
-    //                 Some(c) => c,
-    //             };
-    //
-    //             nodes.push(Node {
-    //                 name: *p,
-    //                 parent: None,
-    //                 children: vec![child],
-    //             });
-    //         }
-    //         Some(_) => {}
-    //     };
-    //
-    //     ;
-    // }
-    //
-    // println!("nodes={:?}", nodes);
-
+    let graph = build_graph(contents.lines());
+    let part_1 = build_order(graph);
+    println!("part_1={}", part_1);
     Ok(())
 }
